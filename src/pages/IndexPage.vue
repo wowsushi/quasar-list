769765483bbf71f062beaa3de686d5c345d8b160
Betty <s9654025@gmail.com>
2024-04-01 14:45:19 +0800

    [feat] implement CRUD flow

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,9 +2,19 @@
   <q-page class="row q-pt-xl">
     <div class="full-width q-px-xl">
       <div class="q-mb-xl">
-        <q-input v-model="tempData.name" label="姓名" />
-        <q-input v-model="tempData.age" label="年齡" />
-        <q-btn color="primary" class="q-mt-md">新增</q-btn>
+        <q-input
+          v-model="tempData.name"
+          label="姓名"
+          :rules="[(val) => val.length > 0]"
+        />
+        <q-input
+          v-model="tempData.age"
+          label="年齡"
+          :rules="[(val) => val.length > 0 && /^\d+$/g.test(val)]"
+        />
+        <q-btn color="primary" class="q-mt-md" @click="handleAddNew"
+          >新增</q-btn
+        >
       </div>
 
       <q-table
@@ -79,13 +89,27 @@
 
 <script setup lang="ts">
 import axios from 'axios';
-import { QTableProps } from 'quasar';
-import { ref } from 'vue';
+import { config } from 'process';
+import { QTableProps, useQuasar } from 'quasar';
+import { onMounted, ref } from 'vue';
+const $q = useQuasar();
+
 interface btnType {
   label: string;
   icon: string;
   status: string;
 }
+interface blockData {
+  name: string;
+  age: number;
+  creatorId: string;
+  id: string;
+}
+const BASE_URL = 'https://demo.mercuryfire.com.tw:49110';
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 60 * 1000,
+});
 const blockData = ref([
   {
     name: 'test',
@@ -119,12 +143,97 @@ const tableButtons = ref([
   },
 ]);
 
-const tempData = ref({
+function doError(message: string) {
+  $q.dialog({
+    title: '發生錯誤',
+    message,
+  });
+}
+async function fetchBlockData() {
+  try {
+    const result = await axiosInstance.get('/crudTest/a');
+    blockData.value = result.data.result;
+  } catch (err) {
+    doError(err.message);
+  }
+}
+async function addBlockData(data: tempData) {
+  try {
+    await axiosInstance.post('/crudTest', {
+      name: data.name,
+      age: +data.age,
+    });
+  } catch (err) {
+    doError(err.message);
+  }
+}
+async function updateBlockData(data: tempData & { id: string }) {
+  try {
+    await axiosInstance.patch('/crudTest', {
+      id: data.id,
+      name: data.name,
+      age: +data.age,
+    });
+  } catch (err) {
+    doError(err.message);
+  }
+}
+async function deleteBlockData(id: string) {
+  try {
+    await axiosInstance.delete(`/crudTest/${id}`);
+  } catch (err) {
+    doError(err.message);
+  }
+}
+
+onMounted(() => {
+  fetchBlockData();
+});
+
+type tempData = {
+  name: string;
+  age: string;
+};
+const tempData = ref<tempData>({
   name: '',
   age: '',
 });
-function handleClickOption(btn, data) {
-  // ...
+async function handleAddNew() {
+  await addBlockData(tempData.value);
+  fetchBlockData();
+  tempData.value = {
+    name: '',
+    age: '',
+  };
+}
+
+function confirmCancel(data: blockData) {
+  $q.dialog({
+    title: '提示',
+    message: '是否確定刪除該筆資料？',
+    cancel: '取消',
+    persistent: true,
+    ok: '確定',
+  }).onOk(async () => {
+    await deleteBlockData(data.id);
+    fetchBlockData();
+  });
+}
+async function handleClickOption(btn: btnType, data: blockData) {
+  switch (btn.status) {
+    case 'edit': {
+      await updateBlockData({
+        id: data.id,
+        ...tempData.value,
+      });
+      fetchBlockData();
+      break;
+    }
+    case 'delete': {
+      confirmCancel(data);
+      break;
+    }
+  }
 }
 </script>
 
